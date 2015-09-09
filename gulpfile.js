1072ce84f35f1e83e15e675a88489fd9650ee61c
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-09 13:16:59 -0700

    Add manifest.json support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp            = require('gulp'),
+    manifest        = require('asset-builder')('./assets/manifest.json'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     plugins         = gulpLoadPlugins();
+
+var path  = manifest.paths, //path.source, path.dest etc
+    globs = manifest.globs; //globs.images, globs.bower etc
 
 function handleError(err) {
   plugins.util.log(plugins.util.colors.red('[ERROR] ' + err.toString()));
