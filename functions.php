b2fc946fddf4ac7c902be88330c7bd6a891f3457
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-09 15:12:01 -0700

    Use the sage way for functions includes.

--- functions.php
+++ functions.php
@@ -1,33 +1,40 @@
 <?php
-/*
- *  Custom functions, support, custom post types and more.
- */
-
-/*------------------------------------*\
-    External Modules/Files
-\*------------------------------------*/
-
 /**
- * Composer Autoload classes
+ * Tofino includes
+ *
+ * The $tofino_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
  */
-require 'vendor/autoload.php';
+$tofino_includes = [
+  'vendor/autoload.php',  // Composer Autoload classes
+];
+foreach ($tofino_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'tofino'), $file), E_USER_ERROR);
+  }
+  require_once $filepath;
+}
+unset($file, $filepath);
 
 /**
  * ACF Filters
 */
 
 //Update ACF settings path
-add_filter('acf/settings/path', 'eb_acf_settings_path');
+add_filter('acf/settings/path', 'tf_acf_settings_path');
 
-function eb_acf_settings_path( $path ) {
+function tf_acf_settings_path( $path ) {
   $path = get_stylesheet_directory() . "/vendor/advanced-custom-fields/advanced-custom-fields-pro/";
   return $path;
 }
 
 //Update ACF settings dir
-add_filter('acf/settings/dir', 'eb_acf_settings_dir');
+add_filter('acf/settings/dir', 'tf_acf_settings_dir');
 
-function eb_acf_settings_dir( $dir ) {
+function tf_acf_settings_dir( $dir ) {
   $dir = get_stylesheet_directory_uri() . "/vendor/advanced-custom-fields/advanced-custom-fields-pro/";
   return $dir;
 }
