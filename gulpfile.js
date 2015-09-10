d9de8bb0e7d794920c2aa3210e192b2d75453a36
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-10 10:42:59 -0700

    Prep for gulptasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp            = require('gulp'),
     manifest        = require('asset-builder')('./assets/manifest.json'),
     gulpLoadPlugins = require('gulp-load-plugins'),
+    merge           = require('merge-stream'),
     plugins         = gulpLoadPlugins();
 
 var path  = manifest.paths, //path.source, path.dest etc
@@ -11,3 +12,46 @@ function handleError(err) {
   plugins.util.beep();
   this.emit('end');
 }
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+  var merged = merge();
+  console.log(manifest);
+  console.log('-------------');
+  manifest.forEachDependency('js', function(dep) {
+    console.log(dep);
+    console.log('--------');
+    merged.add(
+
+      gulp.src(dep.globs, {base: 'scripts', merge: true})
+        .pipe(plugins.concat(dep.name))
+        .pipe(plugins.uglify())
+
+    );
+  });
+  return merged
+
+    //.pipe(gulp.dest(path.dist));
+});
+
+// Min / Crush images
+gulp.task('images', function () {
+  return gulp.src(globs.images)
+    .pipe(plugins.imagemin({ progressive: true }))
+    .pipe(gulp.dest(path.dist + 'images'));
+});
+
+// Convert SVGs to Sprites
+gulp.task('svgs', function () {
+  return gulp.src(globs.svgs)
+    .pipe(plugins.svgmin())
+    .pipe(plugins.svgSprites({
+      mode: "symbols",
+      svgId: "svg-%f",
+      preview: false,
+      svg: {
+        symbols: "svg-sprite.svg"
+      }
+    }))
+    .pipe(gulp.dest(path.dist));
+});
