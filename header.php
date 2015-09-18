341aeed606fad0d392267fe6d61094e320c5291b
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-18 11:28:01 -0700

    Move navbar-brand outside of collapse.

--- header.php
+++ header.php
@@ -8,11 +8,11 @@
 </head>
 <body <?php body_class(); ?>>
 <nav class="navbar navbar-light navbar-fixed-top bg-faded">
+  <a class="navbar-brand" href="<?php echo home_url() ?>"><?php echo bloginfo('name') ?></a>
   <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#main-menu">
     &#9776;
   </button>
   <div id="main-menu" class="collapse navbar-toggleable-xs">
-    <a class="navbar-brand" href="<?php echo home_url() ?>"><?php echo bloginfo('name') ?></a>
     <?php
       wp_nav_menu( array(
         'menu'              => 'nav_menu',
