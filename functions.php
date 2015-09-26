7049f1072f47ba5ea3d71aec475c1f02ee959831
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-25 17:02:25 -0700

    User Composer for autoloading files. Fixes #22.

--- functions.php
+++ functions.php
@@ -11,28 +11,11 @@ $theme_config = [
 /**
  * Tofino includes
  *
- * The $tofino_includes array determines the code library included in your theme.
- * Add or remove files to the array as needed. Supports child theme overrides.
- *
- * Please note that missing files will produce a fatal error.
+ * Library includes are now defined in the composer.json and autloaded.
+ * Use 'composer dump-autoload -o' after adding new files.
  *
  */
-$tofino_includes = [
-  'vendor/autoload.php',  // Composer Autoload classes
-  'lib/assets.php',
-  'lib/init.php',
-  'lib/svg.php',
-  'lib/theme-options.php',
-  'lib/required-plugins.php'
-];
-
-foreach ($tofino_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'tofino'), $file), E_USER_ERROR);
-  }
-  require_once $filepath;
-}
-unset($file, $filepath);
+require_once 'vendor/autload.php'
 
 //TODO: Move this function out of functions.php
 function menu_position() {
