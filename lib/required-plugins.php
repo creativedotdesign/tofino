3af806350e1ff867865b1ddfb14fc317c63b4842
Daniel Hewes <daniel@danimalweb.co.uk>
2015-10-08 08:55:37 -0700

    Disable nag message for required plugins. Fixes #38.

--- lib/required-plugins.php
+++ lib/required-plugins.php
@@ -20,8 +20,8 @@ function required_plugins() {
     'menu'         => 'tgmpa-install-plugins', // Menu slug.
     'parent_slug'  => 'themes.php',            // Parent menu slug.
     'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-    'has_notices'  => true,                    // Show admin notices or not.
-    'dismissable'  => false,                   // If false, a user cannot dismiss the nag message.
+    'has_notices'  => false,                    // Show admin notices or not.
+    'dismissable'  => true,                   // If false, a user cannot dismiss the nag message.
     'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
     'is_automatic' => true,                    // Automatically activate plugins after installation or not.
     'message'      => '',                      // Message to output right before the plugins table.
