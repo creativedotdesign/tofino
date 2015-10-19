2cbdef3fa6148e2bb0ac6529b616b8d5d66d95d9
Daniel Hewes <daniel@danimalweb.co.uk>
2015-10-19 15:40:29 -0700

    Post auto fixed JS file.
    Looks like the auto fixer indented some stuff.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,49 +10,41 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  *
  * ======================================================================== */
-
-(function($) {
-
-// Use this variable to set up the common and page specific functions. If you
-// rename this variable, you will also need to rename the namespace below.
-var tofino = {
-  // JavaScript to be fired on all pages
-  common: {
-    init: function() {
-      svg4everybody();
-
-      $('#tofino-notification .close').on('click', function () {
-        Cookies.set('tofino-notification-closed', 'yes');
+(function ($) {
+  // Use this variable to set up the common and page specific functions. If you
+  // rename this variable, you will also need to rename the namespace below.
+  var tofino = {
+    // JavaScript to be fired on all pages
+    common: {
+      init: function () {
+        svg4everybody();
+        $('#tofino-notification .close').on('click', function () {
+          Cookies.set('tofino-notification-closed', 'yes');
+        });
+      }
+    },
+    // Home page
+    home: {
+      init: function () {
+      }
+    }
+  };
+  // The routing fires all common scripts, followed by the page specific scripts.
+  // Add additional events for more control over timing e.g. a finalize event
+  var UTIL = {
+    fire: function (func, funcname, args) {
+      var namespace = tofino;
+      funcname = funcname === undefined ? 'init' : funcname;
+      if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
+        namespace[func][funcname](args);
+      }
+    },
+    loadEvents: function () {
+      UTIL.fire('common');
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
+        UTIL.fire(classnm);
       });
     }
-  },
-  // Home page
-  home: {
-    init: function() {
-
-    }
-  }
-};
-
-// The routing fires all common scripts, followed by the page specific scripts.
-// Add additional events for more control over timing e.g. a finalize event
-var UTIL = {
-  fire: function(func, funcname, args) {
-    var namespace = tofino;
-    funcname = (funcname === undefined) ? 'init' : funcname;
-    if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-      namespace[func][funcname](args);
-    }
-  },
-  loadEvents: function() {
-    UTIL.fire('common');
-
-    $.each(document.body.className.replace(/-/g, '_').split(/\s+/),function(i,classnm) {
-      UTIL.fire(classnm);
-    });
-  }
-};
-
-$(document).ready(UTIL.loadEvents);
-
-})(jQuery); // Fully reference jQuery after this point.
+  };
+  $(document).ready(UTIL.loadEvents);
+}(jQuery));  // Fully reference jQuery after this point.
