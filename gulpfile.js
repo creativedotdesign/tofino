2b856f0fd4cfd34817d217ac40eef230da8d2b1b
Daniel Hewes <daniel@danimalweb.co.uk>
2015-10-19 14:03:59 -0700

    Add gulp help and descriptions to gulp tasks. Rename some tasks. Fixes #52.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp            = require('gulp'),
+var gulp            = require('gulp-help')(require('gulp'), {hideDepsMessage: true}),
     manifest        = require('asset-builder')('./assets/manifest.json'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     merge           = require('merge-stream'),
@@ -10,6 +10,23 @@ var gulp            = require('gulp'),
 var path    = manifest.paths, //path.source, path.dest etc
     globs   = manifest.globs, //globs.images, globs.bower etc
     config  = manifest.config || {};
+
+var gulpHelp = {
+  styles: 'Compile and concat SCSS to CSS with sourcemaps and autoprefixer. Also runs styles:lint.',
+  stylesLint: 'Lints all SCSS files.',
+  scripts: 'Contact js files with sourcemaps. Also runs scripts:lint.',
+  scriptsLint: 'Lints all js files.',
+  images: 'Compress JPG and PNG files.',
+  svgs: 'Minify SVG files. Also runs svg:sprite.',
+  svgSprite: 'Concat and minify SVG files in to a single SVG sprite file.',
+  fonts: 'Copy the fonts directory to dist.',
+  phpLint: 'Lint theme PHP files based on PSR-2.',
+  phpFix: 'Fix all fixable PHP lint errors. This will override existing files.',
+  clean: 'Deletes the dist directory.',
+  build: 'Main build task. Runs clean, styles, scripts, images, svgs, fonts and php:lint.',
+  watch: 'Watch SCSS, JS, SVG and PHP files. Uses browserSync via proxy.',
+  default: 'Runs the build task.'
+};
 
 //Override standard gulp.src task
 //Use notify and gulp util as error notifcation
@@ -30,7 +47,7 @@ gulp.src = function() {
 };
 
 //Compile SCSS to CSS
-gulp.task('styles', ['sass-lint', 'clean'], function() {
+gulp.task('styles', gulpHelp.styles, ['styles:lint', 'clean'], function() {
   var merged = merge();
 
   manifest.forEachDependency('css', function(dep) {
@@ -58,18 +75,26 @@ gulp.task('styles', ['sass-lint', 'clean'], function() {
       "onLast": true,
       "wait": true
     })));
+}, {
+  options: {
+    'production': 'Minified without sourcemaps.'
+  }
 });
 
 // Lints scss files
-gulp.task('sass-lint', function() {
+gulp.task('styles:lint', gulpHelp.stylesLint, function() {
   return gulp.src(path.source + 'styles/**/*.scss')
     .pipe(plugins.sassLint())
     .pipe(plugins.sassLint.format())
     .pipe(plugins.if(argv.production, plugins.sassLint.failOnError()));
+}, {
+  options: {
+    'production': 'Fail on error.'
+  }
 });
 
 // Concatenate & Minify JS
-gulp.task('scripts', ['jshint', 'clean'], function() {
+gulp.task('scripts', gulpHelp.scripts, ['scripts:lint', 'clean'], function() {
   var merged = merge();
 
   manifest.forEachDependency('js', function(dep) {
@@ -94,18 +119,26 @@ gulp.task('scripts', ['jshint', 'clean'], function() {
       "onLast": true,
       "wait": true
     })));
+}, {
+  options: {
+    'production': 'Minified without sourcemaps.'
+  }
 });
 
 // Lints configuration JSON and project JS.
-gulp.task('jshint', function() {
+gulp.task('scripts:lint', gulpHelp.scriptsLint, function() {
   return gulp.src(['bower.json', 'gulpfile.js', path.scripts + '/**/*.js'])
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'))
     .pipe(plugins.if(argv.production, plugins.jshint.reporter('fail')));
+}, {
+  options: {
+    'production': 'Fail on error.'
+  }
 });
 
 // Min / Crush images
-gulp.task('images', ['clean'], function () {
+gulp.task('images', gulpHelp.images, ['clean'], function () {
   return gulp.src(globs.images)
     .pipe(plugins.imagemin({
       progressive: true,
@@ -115,8 +148,15 @@ gulp.task('images', ['clean'], function () {
     .pipe(plugins.if(!argv.production, plugins.notify("Images task complete")));
 });
 
+//Minify SVGS + run sprite task
+gulp.task('svgs', gulpHelp.svgs, ['svg:sprite'], function () {
+  return gulp.src(path.svgs + '*.svg')
+    .pipe(plugins.svgmin())
+    .pipe(gulp.dest(path.dist + 'svg'));
+});
+
 // Convert SVGs to Sprites
-gulp.task('svg-sprite', ['clean'], function () {
+gulp.task('svg:sprite', gulpHelp.svgSprite, ['clean'], function () {
   return gulp.src(path.svgs + 'sprites/*.svg')
     .pipe(plugins.svgmin())
     .pipe(plugins.svgSprite({
@@ -130,21 +170,14 @@ gulp.task('svg-sprite', ['clean'], function () {
     .pipe(plugins.if(!argv.production, plugins.notify("SVG task complete")));
 });
 
-//Minify SVGS + run sprite task
-gulp.task('svgs', ['svg-sprite'], function () {
-  return gulp.src(path.svgs + '*.svg')
-    .pipe(plugins.svgmin())
-    .pipe(gulp.dest(path.dist + 'svg'));
-});
-
 //Copy font files from assets to dist
-gulp.task('fonts', ['clean'], function () {
+gulp.task('fonts', gulpHelp.fonts, ['clean'], function () {
   return gulp.src(path.fonts + '*')
     .pipe(gulp.dest(path.dist + 'fonts'));
 });
 
 //Lint PHP files using ruleset.xml.
-gulp.task('php:lint', function () {
+gulp.task('php:lint', gulpHelp.phpLint, function () {
   return gulp.src(['**/*.php', '!vendor/**/*.*', '!tests/**/*.*'])
     .pipe(plugins.phpcs({ // Validate files using PHP Code Sniffer
       bin: 'vendor/bin/phpcs',
@@ -155,7 +188,7 @@ gulp.task('php:lint', function () {
 });
 
 //Fix PHP based on ruleset.xml. This will override existing PHP files
-gulp.task('php:fix', function () {
+gulp.task('php:fix', gulpHelp.phpFix, function () {
   return gulp.src(['**/*.php', '!vendor/**/*.*', '!tests/**/*.*'])
     .pipe(plugins.phpcbf({
       bin: 'vendor/bin/phpcbf',
@@ -167,13 +200,13 @@ gulp.task('php:fix', function () {
 });
 
 // Deletes the build folder entirely.
-gulp.task('clean', require('del').bind(null, [path.dist]));
+gulp.task('clean', gulpHelp.clean, require('del').bind(null, [path.dist]));
 
 // Generic build task. Use with '--production' for minified js / css
-gulp.task('build', ['clean', 'images', 'svgs', 'styles', 'scripts', 'fonts', 'php:lint']);
+gulp.task('build', gulpHelp.build, ['clean', 'images', 'svgs', 'styles', 'scripts', 'fonts', 'php:lint']);
 
 // Watch Files For Changes
-gulp.task('watch', function() {
+gulp.task('watch', gulpHelp.watch, function() {
 
   var ghost = false;
 
@@ -206,6 +239,10 @@ gulp.task('watch', function() {
     plugins.util.log('File Changed: ' + file.path + '');
   });
 
+}, {
+  options: {
+    'chill': 'Do not pass clicks, forms or scroll to other browsers.'
+  }
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', gulpHelp.default, ['build']);
