8efb4b94d518fd1c7ab39f0871825cfd1b8726e8
Daniel Hewes <daniel@danimalweb.co.uk>
2015-10-22 08:47:47 -0700

    Update single.php to search for template based on post name. Fixes #59 For @mrchimp

--- single.php
+++ single.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<?php
+$template = get_post_type() . '-' . get_page_name(); // (post)-(my-post-name).
+
+if (locate_template('templates/content-single-' . $template . '.php') != '') {
+  get_template_part('templates/content-single', $template); // e.g. templates/content-single-post-my-post-name.php
+} else {
+  get_template_part('templates/content-single', get_post_type());
+} ?>
 
 <?php get_footer(); ?>
