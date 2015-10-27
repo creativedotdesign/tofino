8d919e32d515776fbc457964419e1f73d522df29
Daniel Hewes <daniel@danimalweb.co.uk>
2015-10-27 11:47:39 -0700

    Update to notifications.

--- lib/theme-options.php
+++ lib/theme-options.php
@@ -406,13 +406,13 @@ function notification($position) {
   if ($position == ot_get_option('notification_position')) {
     if (ot_get_option('notification_text') && !isset($_COOKIE['tofino-notification-closed'])) : ?>
       <!-- Notifcation <?php echo $position; ?> -->
-      <div class="alert alert-info fixed-bottom" id="tofino-notification">
+      <div class="alert alert-info notification <?php echo $position; ?>" id="tofino-notification">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
           <span class="sr-only">Close</span>
         </button>
-        <?php echo ot_get_option('notification_text') ?>
+        <p><?php echo ot_get_option('notification_text') ?></p>
       </div><?php
     endif;
   }
-} 
+}
