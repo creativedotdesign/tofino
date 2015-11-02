ebfde7fc159be04a5bae1b68d9cbdd72643ecbf9
Daniel Hewes <daniel@danimalweb.co.uk>
2015-11-02 11:44:52 -0800

    Update columns for responsive.

--- templates/content-page-contact.php
+++ templates/content-page-contact.php
@@ -1,7 +1,7 @@
 <main>
   <div class="container">
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col-xs-12 col-sm-6">
         <?php while (have_posts()) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
 
@@ -45,7 +45,7 @@
           </form>
         <?php endwhile; ?>
       </div>
-      <div class="col-sm-4 col-sm-offset-2">
+      <div class="col-xs-12 col-sm-4 col-sm-offset-2">
         <address>
         <?php echo nl2br(ot_shortcode('address')); ?>
         </address>
