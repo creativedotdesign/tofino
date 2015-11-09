736673ee6908ef6db52e389aaddc25a68ba0d258
Daniel Hewes <daniel@danimalweb.co.uk>
2015-11-09 15:01:13 -0800

    Add feature. Make Urls relative.

---
+++ lib/relative-urls.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tofino\RelativeUrls;
+
+function relative_urls() {
+
+  if (is_admin() || preg_match('/sitemap(_index)?\.xml/', $_SERVER['REQUEST_URI']) || in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php'])) {
+    return;
+  }
+
+  $filters = array(
+    'post_link',
+    'post_type_link',
+    'page_link',
+    'attachment_link',
+    'get_shortlink',
+    'post_type_archive_link',
+    'get_pagenum_link',
+    'get_comments_pagenum_link',
+    'term_link',
+    'search_link',
+    'day_link',
+    'month_link',
+    'year_link',
+  );
+
+  foreach ($filters as $filter) {
+    add_filter($filter, 'wp_make_link_relative');
+  }
+
+}
+
+add_action('template_redirect', __NAMESPACE__ . '\\relative_urls');
