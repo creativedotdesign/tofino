90e9b464399561c4f2ed1d17a9beabe616b112ec
Daniel Hewes <daniel@lambdacreatives.com>
2015-12-06 12:28:09 -0800

    Fix coding standard.

--- lib/helpers.php
+++ lib/helpers.php
@@ -7,7 +7,7 @@ namespace Tofino\Helpers;
  */
 function get_page_name($page_id = null) {
   global $pagename;
-  if (! $pagename) { //Not found in the query_var. Permalinks is probably not enabled.
+  if (!$pagename) { // Not found in the query_var. Permalinks is probably not enabled.
     global $wp_query;
     $page_id  = ($page_id ? $page_id : get_the_ID());
     $post     = $wp_query->get_queried_object();
