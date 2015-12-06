ab9b69e831a9ae32dc064451e1bb70b372b3055d
Daniel Hewes <daniel@lambdacreatives.com>
2015-12-06 12:27:54 -0800

    Remove content prefix from templates.

--- archive.php
+++ archive.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part('templates/content-archive', get_post_type()); ?>
+<?php get_template_part('templates/archive', get_post_type()); ?>
 
 <?php get_footer(); ?>
