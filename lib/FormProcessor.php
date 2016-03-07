5dc45cd69fc0365c8788a654c70f5e8ecdd47847
Daniel Hewes <daniel@lambdacreatives.com>
2016-03-07 12:16:33 -0800

    Update class.

--- lib/FormProcessor.php
+++ lib/FormProcessor.php
@@ -20,9 +20,9 @@ class FormProcessor
     $this->form_data['date_time'] = time(); // Create timestamp for submission
   }
 
-  private function nonceCheck()
+  private function isValidNonce($nonce)
   {
-    if (!wp_verify_nonce($this->data['nextNonce'], 'next_nonce')) { // Compare Nonce from POST request to server generated nonce
+    if (!wp_verify_nonce($nonce, 'next_nonce')) { // Compare Nonce from POST request to server generated nonce
       $this->response['message'] = __('Security check failed.', 'tofino');
       return false;
     } else {
@@ -30,19 +30,26 @@ class FormProcessor
     }
   }
 
-  private function captchaCheck()
+  private function isCaptchaEnabled()
   {
-    // Captcha secret check
-    if (ot_get_option('captcha_secret')) { //Get from theme options
-      $secret = ot_get_option('captcha_secret');
+    if (ot_get_option('enable_captcha_checkbox')) {
+      if (ot_get_option('captcha_secret') && ot_get_option('captcha_site_key')) {
+        return true;
+      } else {
+        $this->response['message'] = __('reCaptcha sitekey and/or secret not found. Set this up in the theme options.', 'tofino');
+        return false;
+      }
     } else {
-      $this->response['message'] = __('reCaptcha sitekey and/or secret not found. Set this up in the theme options.', 'tofino');
       return false;
     }
+  }
 
+  private function isValidCaptcha($captcha_repsonse)
+  {
     // Captcha validation check
+    $secret    = ot_get_option('captcha_secret');
     $recaptcha = new \ReCaptcha\ReCaptcha($secret);
-    $resp = $recaptcha->verify($this->data['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+    $resp      = $recaptcha->verify($captcha_repsonse, $_SERVER['REMOTE_ADDR']);
     if (!$resp->isSuccess()) {
       $errors = $resp->getErrorCodes(); // Should we send some real error codes back to the user?
       $this->response['message'] = __('Captcha failed.', 'tofino');
@@ -52,48 +59,54 @@ class FormProcessor
     }
   }
 
-  public function validateEmailAddress()
+  private function isValidEmail($email_address)
   {
-    if (array_key_exists('email', $this->form_data)) { // Found email field
-      $emailArray = explode('@', $this->form_data['email']); // Split it on @
-      $hostname   = $emailArray[1];
-      if (filter_var($this->form_data['email'], FILTER_VALIDATE_EMAIL) === false || checkdnsrr($hostname, 'MX') === false || gethostbyname($hostname) === $hostname) { //DNS lookup of MX or A/CNAME record
-        $this->response['message'] = __('Invalid email address.', 'tofino');
-        return false;
-      } else {
-        return true;
-      }
+    $emailArray = explode('@', $email_address); // Split it on @
+    $hostname   = $emailArray[1];
+    if (filter_var($email_address, FILTER_VALIDATE_EMAIL) === false || checkdnsrr($hostname, 'MX') === false || gethostbyname($hostname) === $hostname) { //DNS lookup of MX or A/CNAME record
+      $this->response['message'] = __('Invalid email address.', 'tofino');
+      return false;
+    } else {
+      return true;
     }
   }
 
   /**
-   * Get receipient from theme options
+   * Get recipient from theme options
    */
-  private function getSettings()
+  public function getRecipient($theme_option_field)
   {
-    $prefix = $this->data['name'];
-    $settings = [];
-
-    $settings['recipient']    = getRecipient();
-    $settings['subject']      = (ot_get_option($prefix . '_form_email_subject') ? ot_get_option($prefix . '_form_email_subject') : null);
-    $settings['recipient_cc'] = (ot_get_option($prefix . '_form_cc_address') ? ot_get_option($prefix . '_form_cc_address') : null);
-    $settings['send_from']    = (ot_get_option($prefix . '_form_from_address') ? ot_get_option($prefix . '_form_from_address') : null);
-
-    if (ot_get_option($prefix . '_form_to_address')) { // Email address from contact form options
-      $recipient = ot_get_option($prefix . '_form_to_address');
+    if (ot_get_option($theme_option_field)) { // Email address from contact form options
+      $recipient = ot_get_option($theme_option_field);
     } elseif (ot_get_option('email_address')) { // Email address from general options
       $recipient = ot_get_option('email_address');
     } else {
       $this->response['message'] = __('No recipient email address.', 'tofino');
-      return false;
+      return $this->sendResponse($this->response);
     }
     return $recipient;
   }
 
   /**
+   * Save form data as post meta
+   */
+  public function saveData($post_id, $meta_key, $data = array())
+  {
+    if ($post_id && is_array($data)) {
+      $meta_id = add_post_meta($post_id, $meta_key, $data, false);
+      if (0 < intval($meta_id)) {
+        return $meta_id;
+      } else {
+        $this->response['message'] =  __('Unable to save data.', 'tofino');
+        return false;
+      }
+    }
+  }
+
+  /**
    * Genereate email body using html template
    */
-  private function buildEmailBody($template_file = 'contact-form.html')
+  private function buildEmailBody($template = 'contact-form.html')
   {
     if (is_array($this->form_data)) {
       if (array_key_exists('g-recaptcha-response', $this->form_data)) { // Remove reCaptcha from message content
@@ -108,7 +121,7 @@ class FormProcessor
       }
     }
 
-    $message = file_get_contents(get_template_directory() . '/templates/email/' . $template_file); // Get the template.
+    $message = file_get_contents(get_template_directory() . '/templates/email/' . $template); // Get the template.
 
     if (ot_get_option('admin_login_logo_id')) {
       $src     = wp_get_attachment_image_src(ot_get_option('admin_login_logo_id'), 'original');
@@ -127,23 +140,21 @@ class FormProcessor
   /**
    * Send mail. Uses PHPMailer.
    */
-  public function sendMail($recipient, $subject, $recipient_cc = null, $from = null, $template = null)
+  private function sendMail($settings)
   {
-    $settings = getFormSettings();
-
     $headers = ['Content-Type: text/html; charset=UTF-8'];
 
-    if ($from) {
-      $headers[] = 'From: ' . $from;
+    if (array_key_exists('from', $settings)) {
+      $headers[] = 'From: ' . $settings['from'];
     }
 
-    if ($recipient_cc) {
-      $headers[] = 'Cc: ' . $recipient_cc;
+    if (array_key_exists('cc', $settings)) {
+      $headers[] = 'Cc: ' . $settings['cc'];
     }
 
-    $email_body = buildEmailBody($template);
-
-    $mail = wp_mail($recipient, $subject, $email_body, $headers);
+    $template   = (array_key_exists('template', $settings) ? $settings['template'] : null); // @todo: Fix!
+    $email_body = $this->buildEmailBody();
+    $mail       = wp_mail($settings['to'], $settings['subject'], $email_body, $headers);
 
     if ($mail) {
       return true;
@@ -178,21 +189,27 @@ class FormProcessor
    * [process description]
    * @return boolean Success status
    */
-  public function process()
+  public function process($settings)
   {
     // nonce check
-    if (!$this->nonceCheck()) {
+    if (!$this->isValidNonce($this->data['nextNonce'])) {
       return $this->sendResponse($this->response);
     }
 
     // captcha check
-    if (!$this->captchaCheck()) {
-      return $this->sendResponse($this->response);
+    if (array_key_exists('g-recaptcha-response', $this->data)) {
+      if ($this->isCaptchaEnabled()) {
+        if (!$this->isValidCaptcha($this->data['g-recaptcha-response'])) {
+          return $this->sendResponse($this->response);
+        }
+      }
     }
 
     // email address check
-    if (!$this->validateEmailAddress()) {
-      return $this->sendResponse($this->response);
+    if (array_key_exists('email', $this->form_data)) { // Found email field
+      if (!$this->isValidEmail($this->form_data['email'])) {
+        return $this->sendResponse($this->response);
+      }
     }
 
     foreach ($this->callbacks as $callback) {
@@ -203,36 +220,13 @@ class FormProcessor
       }
     }
 
-    if (!$this->getRecipient()) {
+    if (!$this->sendMail($settings)) {
       return $this->sendResponse($this->response);
-    }
-
-
-    // send email?
-    $this->response['success'] = true;
-
-    return $this->response;
-  }
-}
-
-
-
-
-
-/**
- * Save form data as post meta
- */
-function save_form_data($post_id, $meta_key, $data = array()) {
-  if ($post_id && is_array($data)) {
-    $meta_id = add_post_meta($post_id, $meta_key, $data, false);
-    if (0 < intval($meta_id)) {
-      return $meta_id;
     } else {
-      $response = array(
-        'success' => false,
-        'message' => __('Unable to save data.', 'tofino')
-      );
-      \Tofino\Helpers\send_json_response($response);
+      $this->response['message'] = $settings['success_msg'];
+      $this->response['success'] = true;
     }
+
+    return $this->sendResponse($this->response);
   }
 }
