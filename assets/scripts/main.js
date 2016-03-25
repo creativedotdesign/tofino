5f880d0fef34c557d93bc2f1d23db0880761ce81
Daniel Hewes <daniel@lambdacreatives.com>
2016-03-24 17:41:15 -0700

    Remove wrapper function.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,72 +23,68 @@ window.Tether = Tether; //Temp until new BS4 release.
  * Only fires on body classes that match. If a body class contains a dash,
  * replace the dash with an underscore when adding it to the object below.
  *
- * .noConflict()
- * The routing is enclosed within an anonymous function so that you can
- * always reference jQuery with $, even when in .noConflict() mode.
- *
  * ======================================================================== */
-(function($) {
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var tofino = {
-    // JavaScript to be fired on all pages
-    common: {
-      init: function() {
-        //Iniitalize svg4everybody
-        svg4everybody();
 
-        //List for notication close
-        $('#tofino-notification .close').on('click', function() {
-          if (tofinoJS.cookieExpires) {
-            Cookies.set('tofino-notification-closed', 'yes', {expires: parseInt(tofinoJS.cookieExpires)});
+// Use this variable to set up the common and page specific functions. If you
+// rename this variable, you will also need to rename the namespace below.
+var tofino = {
+  // JavaScript to be fired on all pages
+  common: {
+    init: function() {
+
+      //Iniitalize svg4everybody
+      svg4everybody();
+
+      //List for notication close
+      $('#tofino-notification .close').on('click', function() {
+        if (tofinoJS.cookieExpires) {
+          Cookies.set('tofino-notification-closed', 'yes', {expires: parseInt(tofinoJS.cookieExpires)});
+        } else {
+          Cookies.set('tofino-notification-closed', 'yes');
+        }
+      });
+
+      //Assign sticky
+      var $sticky = $('.navbar-sticky-top');
+
+      if ($sticky.length) {
+        //Sticky polyfill for css position: sticky
+        $sticky.Stickyfill();
+
+        //Assign stick offset
+        var stickyTop = $sticky.offset().top;
+
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > stickyTop) {
+            $sticky.addClass('stuck');
           } else {
-            Cookies.set('tofino-notification-closed', 'yes');
+            $sticky.removeClass('stuck');
           }
         });
-
-        //Assign sticky
-        var $sticky = $('.navbar-sticky-top');
-
-        if ($sticky.length) {
-          //Sticky polyfill for css position: sticky
-          $sticky.Stickyfill();
-
-          //Assign stick offset
-          var stickyTop = $sticky.offset().top;
-
-          $(window).scroll(function() {
-            if ($(this).scrollTop() > stickyTop) {
-              $sticky.addClass('stuck');
-            } else {
-              $sticky.removeClass('stuck');
-            }
-          });
-        }
-      }
-    },
-    // Home page
-    home: {
-      init: function() {
       }
     }
-  };
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var namespace = tofino;
-      funcname = funcname === undefined ? 'init' : funcname;
-      if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      UTIL.fire('common');
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-      });
+  },
+  // Home page
+  home: {
+    init: function() {
     }
-  };
-  $(document).ready(UTIL.loadEvents);
-}(jQuery));  // Fully reference jQuery after this point.
+  }
+};
+// The routing fires all common scripts, followed by the page specific scripts.
+// Add additional events for more control over timing e.g. a finalize event
+var UTIL = {
+  fire: function(func, funcname, args) {
+    var namespace = tofino;
+    funcname = funcname === undefined ? 'init' : funcname;
+    if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
+      namespace[func][funcname](args);
+    }
+  },
+  loadEvents: function() {
+    UTIL.fire('common');
+    $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
+      UTIL.fire(classnm);
+    });
+  }
+};
+$(document).ready(UTIL.loadEvents);
