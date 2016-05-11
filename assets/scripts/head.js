b7956b96f018e4246011b1131eaf3471174718bf
Daniel Hewes <daniel@lambdacreatives.com>
2016-05-11 14:56:28 +0100

    Remove custom example.

--- assets/scripts/head.js
+++ assets/scripts/head.js
@@ -4,12 +4,6 @@ global.loadCSS = fgLoadcss.loadCSS; // Make it global
 
 import WebFont from 'webfontloader';
 
-// Custom font example.
-// WebFont.load({
-//   custom: {
-//     families: ['Montserrat']
-//   }
-// });
 
 // Google font example.
 WebFont.load({
