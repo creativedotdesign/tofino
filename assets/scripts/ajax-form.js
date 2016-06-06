9ff00a3e0a300e643d8a88cb0c5fafa46d962e12
Daniel Hewes <daniel@lambdacreatives.com>
2016-06-06 11:42:46 -0700

    Reactor script, prevent multiple submissions, tidy up code.

--- assets/scripts/ajax-form.js
+++ assets/scripts/ajax-form.js
@@ -23,99 +23,107 @@
   }
 }(function ($) {
   $.fn.ajaxForm = function (options) {
+    $(this).on('submit', function(e) {
+      e.preventDefault(); // Don't really submit.
 
-    $(this).each(function () {
-      var request;
+      if ($(this).hasClass('submitting')) {
+        return false;
+      }
 
-      $(this).on('submit', function(e) {
+      $(this).addClass('submitting');
 
-        e.preventDefault(); // Don't really submit.
+      var defaults = { //Defaults
+        responseDiv: '.js-form-result',
+        action: $(this).data('wp-action'), //The PHP function name to call via AJAX
+        btnProgressText: 'Wait..',
+        beforeSerializeData: function() {},
+        beforeRedirect: function() {},
+        afterSuccess: function() {}
+      };
 
-        var defaults = { //Defaults
-          responseDiv: '.js-form-result',
-          action: $(this).data('wp-action'), //The PHP function name to call via AJAX
-          btnProgressText: 'Wait..',
-          beforeSerializeData: function() {},
-          beforeRedirect: function() {},
-          afterSuccess: function() {}
-        };
+      var opts = $.extend({}, defaults, options);
 
-        var opts = $.extend({}, defaults, options);
+      opts.responseDiv = $(opts.responseDiv);
 
-        opts.responseDiv = $(opts.responseDiv);
+      var $form = $(this);
 
-        // var request,
-        var $form = $(this);
+      opts.beforeSerializeData(); // Callback function
 
-        if (request) { // If request exists, bail.
-          request.abort();
+      var serializedData  = $(this).serialize(),
+          $btnSubmit      = $(this).find(':submit'),
+          btnOrgText      = $btnSubmit.text(), // Get original text value
+          btnProgressText = opts.btnProgressText;
+
+      $(this).find(':input').prop('disabled', true); // Set the disabled state
+      $btnSubmit.text(btnProgressText); // Set in progress text
+
+      var request = $.post(
+        tofinoJS.ajaxUrl, {
+          action: opts.action, // Passed to WP for the ajax action
+          data: serializedData,
+          nextNonce: tofinoJS.nextNonce
         }
+      );
 
-        opts.beforeSerializeData(); // Callback function
-
-        var serializedData  = $(this).serialize(),
-            $btnSubmit      = $(this).find(':submit'),
-            btnOrgText      = $btnSubmit.text(), // Get original text value
-            btnProgressText = opts.btnProgressText;
-
-        $(this).find(':input').prop('disabled', true); // Set the disabled state
-        $btnSubmit.text(btnProgressText); // Set in progress text
-
-        request = $.post(
-          tofinoJS.ajaxUrl, {
-            action: opts.action, // Passed to WP for the ajax action
-            data: serializedData,
-            nextNonce: tofinoJS.nextNonce
+      request.done(function(response) {
+        $form.removeClass('submitting');
+        if (response.success === true) {
+          if (response.redirect) {
+            opts.beforeRedirect(); // Callback function
+            window.location = response.redirect;
+            return false;
           }
-        );
 
-        request.done(function(response, textStatus, errorThrown) { // eslint-disable-line
-          if (response.success === true) {
-            if (response.redirect) {
-              opts.beforeRedirect(); // Callback function
-              window.location = response.redirect;
-              return false;
-            }
+          opts.responseDiv
+            .removeClass('alert-danger')
+            .addClass('alert alert-success')
+            .html(response.message);
 
-            opts.responseDiv.removeClass('alert-danger').addClass('alert alert-success').html(response.message);
-            $form.find(':input').val(''); // Reset fields.
-            $form.find(':submit').text(btnOrgText); // Set send button text back to default
-            $form.hide(); // Hide form
+          $form.find(':input').val(''); // Reset fields.
+          $form.find(':submit').text(btnOrgText); // Set send button text back to default
+          $form.hide(); // Hide form
 
-            opts.afterSuccess(); // Callback function
-          } else {
-            opts.responseDiv.addClass('alert alert-danger').html(response.message);
-            $form.find(':input').prop('disabled', false); // Re-enable fields
-            $form.find(':submit').text(btnOrgText); // Reset submit btn to org text
+          opts.afterSuccess(); // Callback function
+        } else {
+          opts.responseDiv
+            .addClass('alert alert-danger')
+            .html(response.message);
 
-            // Remove any existing failed validation classes
-            $form.find('.form-control-danger').removeClass('form-control-danger');
-            $form.find('.has-danger').removeClass('has-danger');
+          $form.find(':input').prop('disabled', false); // Re-enable fields
+          $form.find(':submit').text(btnOrgText); // Reset submit btn to org text
 
-            var inputs = $form.find(':input')
-              .not(':input[type=button], :input[type=submit], :input[type=reset]'); // Select all inputs not buttons
+          // Remove any existing failed validation classes
+          $form.find('.form-control-danger').removeClass('form-control-danger');
+          $form.find('.has-danger').removeClass('has-danger');
 
-            inputs.addClass('form-control-success'); // All valid / green. Server only returns invalid fields
-
-            $(inputs).each(function() {
+          $form.find(':input')
+            .not(':input[type=button], :input[type=submit], :input[type=reset]') // Select all inputs not buttons
+            .addClass('form-control-success') // All valid / green. Server only returns invalid fields
+            .each(function() {
               $(this).closest('.form-group').addClass('has-success');
             });
 
-            if (response.type === 'validation') {
-              var invalidFields = $.parseJSON(response.extra);
-              $.each(invalidFields, function(key) {
-                $form.find('[name=' + key + ']').removeClass('form-control-success').addClass('form-control-danger')
-                  .closest('.form-group').removeClass('has-success').addClass('has-danger');
-              });
-            }
+          if (response.type === 'validation') {
+            var invalidFields = $.parseJSON(response.extra);
+            $.each(invalidFields, function(key) {
+              $form.find('[name=' + key + ']')
+                .removeClass('form-control-success')
+                .addClass('form-control-danger');
+              $form.closest('.form-group')
+                .removeClass('has-success')
+                .addClass('has-danger');
+            });
           }
-        });
+        }
+      });
 
-        request.fail(function(response, textStatus, errorThrown) { // eslint-disable-line
-          opts.responseDiv.addClass('alert alert-danger').html('An error occured.');
-          $form.find(':input').prop('disabled', false); // Re-enable fields
-          $form.find(':submit').text(btnOrgText); // Reset submit btn
-        });
+      request.fail(function() {
+        opts.responseDiv
+          .addClass('alert alert-danger')
+          .html('An error occured.');
+        $form.find(':input').prop('disabled', false); // Re-enable fields
+        $form.find(':submit').text(btnOrgText); // Reset submit btn
+        $form.removeClass('submitting');
       });
     });
   };
