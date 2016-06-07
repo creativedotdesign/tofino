003524ffcac83bc7590e82f8d855c8ed809cccf8
Daniel Hewes <daniel@lambdacreatives.com>
2016-06-07 14:56:19 -0700

    Update DOM based routing to ES6 export and move in to separate file.
    Added bonus of using finalize event. Code from Roots/Sage project.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,9 @@
 // Assign jQuery to $
 var $ = window.jQuery;
 
+// Import router
+import Router from './router.js';
+
 // Import svg4everybody
 import svg4everybody from 'svg4everybody';
 
@@ -43,15 +46,6 @@ import "bootstrap/dist/js/umd/dropdown.js"; // Menu dropdown
 // import "bootstrap/dist/js/umd/tab.js";
 // import "bootstrap/dist/js/umd/tooltip.js"; // Requires tether
 // import "bootstrap/dist/js/umd/popover.js"; // Requires tether
-
-/* ========================================================================
- * DOM-based Routing
- * Based on http://goo.gl/EUTi53 by Paul Irish
- *
- * Only fires on body classes that match. If a body class contains a dash,
- * replace the dash with an underscore when adding it to the object below.
- *
- * ======================================================================== */
 
 // Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
@@ -104,24 +98,15 @@ var tofino = {
   // Home page
   home: {
     init: function() {
+      // JavaScript to be fired on the home page
+    },
+    finalize: function() {
+      // JavaScript to be fired on the home page, after the init JS
     }
   }
 };
-// The routing fires all common scripts, followed by the page specific scripts.
-// Add additional events for more control over timing e.g. a finalize event
-var UTIL = {
-  fire: function(func, funcname, args) {
-    var namespace = tofino;
-    funcname = funcname === undefined ? 'init' : funcname;
-    if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-      namespace[func][funcname](args);
-    }
-  },
-  loadEvents: function() {
-    UTIL.fire('common');
-    $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-      UTIL.fire(classnm);
-    });
-  }
-};
-$(document).ready(UTIL.loadEvents);
+
+// Load Events
+$(document).ready(function() {
+  new Router(tofino).loadEvents();
+});
