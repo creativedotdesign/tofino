1948055aca98926c54365bf7a4d94cf7453145aa
Daniel Hewes <daniel@lambdacreatives.com>
2016-10-10 17:01:35 -0700

    Add the content?

--- templates/content-page-contact.php
+++ templates/content-page-contact.php
@@ -5,6 +5,8 @@
         <?php while (have_posts()) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
 
+          <?php the_content(); ?>
+
           <!-- Empty div for form response -->
           <div class="js-form-result"></div>
 
