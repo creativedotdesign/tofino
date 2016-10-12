ba081b35a4beba2a241812189b775fe3b29799c8
Daniel Hewes <daniel@lambdacreatives.com>
2016-10-12 15:13:34 -0700

    Fix PSI task callback / process.exit.

--- gulp/psi.js
+++ gulp/psi.js
@@ -6,14 +6,18 @@ module.exports = function (gulp, slug, mobile) {
     'psi',
     'Google Page Speed Insights.',
     ['browser-sync', 'ngrok'],
-    function() {
+    function(cb) {
       var site = process.env.URL  + '/' + slug;
       console.log('Running Google PSI against URL: ' + site);
-      psi.output(site, {
+      return psi.output(site, {
         nokey: 'true',
-        strategy: (mobile ? 'mobile' : 'desktop')
-      }).then(function() {
-        process.exit();
+        strategy: (mobile ? 'mobile' : 'desktop'),
+        // Set to low value as default 70 throws un-catchable error for
+        // threshold not met due to local speed issues.
+        threshold: 50
+      }).then(function () {
+        cb();
+        process.exit(1);
       });
     }
   );
