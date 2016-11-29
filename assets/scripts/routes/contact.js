1141de1c9a959b3cd3a7e2768d3fe974ae3fdd53
Daniel Hewes <daniel@lambdacreatives.com>
2016-11-29 10:06:54 -0800

    Remove callback functions from contact form js demo code.

--- assets/scripts/routes/contact.js
+++ assets/scripts/routes/contact.js
@@ -4,14 +4,7 @@ var $ = window.jQuery;
 export default {
   init() {
     // JavaScript to be fired on contact page page
-    $('.contact-form').ajaxForm({
-      beforeSerializeData: function() { // Before data serialize callback
-      },
-      afterSucess: function() { // Success callback function.
-      },
-      afterError: function() { // Error callback function
-      }
-    });
+    $('.contact-form').ajaxForm();
   },
   loaded() {
     // Javascript to be fired on page once fully loaded
