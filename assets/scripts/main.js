90323cb8688ce9c161bbb9463d13a8798d344778
Daniel Hewes <daniel@lambdacreatives.com>
2017-01-30 23:41:56 -0800

    Fix typo.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,7 +15,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 // rename this variable, you will also need to rename the namespace below.
 // You add additional pages to this array by referencing the the body class
 // and creating the js file in the routes directory. Remember to import the
-// file as per the common exanple near the top of this file.
+// file as per the common example near the top of this file.
 const routes = {
   // All pages
   common,
