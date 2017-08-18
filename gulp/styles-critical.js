6df93e668fe610107aefe7960c14f40733d96e32
Daniel Hewes <daniel@Daniels-MacBook-Pro.local>
2017-08-18 14:21:14 -0700

    Update critical styles task Fixes #238

--- gulp/styles-critical.js
+++ gulp/styles-critical.js
@@ -1,21 +1,22 @@
 var manifest = require('../assets/manifest.json'),
-    critical = require('critical'),
-    gulpif   = require('gulp-if');
+    critical = require('critical');
 
 // Critical path css
-module.exports = function (gulp, stagingUrl) {
+module.exports = function (gulp, util) {
   'use strict';
-  var paths = manifest.paths,
-  config    = manifest.config;
+  var paths = manifest.paths;
   gulp.task(
     'styles:critical',
     'Generates the Critical CSS file based on dimentions in the array.', [
       'styles',
-      'styles:lint'
+      'styles:lint',
+      'browser-sync',
+      'ngrok'
     ],
     function() {
+      var site = process.env.URL;
       return critical.generate({
-        src: gulpif(stagingUrl, config.stagingUrl, config.devUrl),
+        src: site,
         dest: paths.dist + '/css/critical.css',
         ignore: ['@font-face',/url\(/],
         // pathPrefix: '/wp-content/themes/tofino/' + path.dist + 'fonts',
@@ -27,6 +28,9 @@ module.exports = function (gulp, stagingUrl) {
           height: 900,
           width: 1200
         }]
+      }).done(function () {
+        util.log(util.colors.green('Critical CSS Generated!'));
+        process.exit();
       });
     }
   );
