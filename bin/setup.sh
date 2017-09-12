c0e4de98e0adbb8a342a70ec931018875d9650cb
Daniel Hewes <danimalweb@gmail.com>
2017-09-12 10:56:28 -0700

    Update with precommit hook for linting including new Gulp task. Setup script tweaks and composer lock updated.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+. ~/.nvm/nvm.sh # Include reference to NVM
+
+echo 'Composer install deps...'
 composer install
-npm install
-gulp
+
+echo 'Install and use Node v6.7...'
+nvm install 6.7.0
+nvm use 6.7
+
+echo 'Install Yarn from npm...'
+npm install -g yarn
+
+echo 'Yarn install deps...'
+yarn -v
+yarn install
+
+echo 'Run first build...'
+npm build
