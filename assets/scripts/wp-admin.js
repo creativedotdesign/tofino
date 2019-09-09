ab767002930a5294d473cfdbf6fd9b4193f0d707
Daniel <daniel@Daniels-iMac.local>
2019-09-09 06:44:17 -0700

    Formatting cleanup.

--- assets/scripts/wp-admin.js
+++ assets/scripts/wp-admin.js
@@ -1,8 +1,8 @@
 import Cookies from 'js-cookie';
 
-(function($) {
-  $(document).ready(function() {
-    $('.maintenance-mode-alert button').on('click', function() {
+(function ($) {
+  $(document).ready(function () {
+    $('.maintenance-mode-alert button').on('click', function () {
       Cookies.set('tofino_maintenance_alert_dismissed', 'true');
       $('.maintenance-mode-alert').fadeOut('fast');
     });
