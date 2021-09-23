a51c1f7711846f18ce79ae0659df6ad3fc888551
Daniel Hewes <daniel@creativedotdesign.com>
2021-09-23 16:16:17 -0700

    feat: Footer Menu Support fixes #298.

--- footer.php
+++ footer.php
@@ -5,6 +5,25 @@ if (get_theme_mod('footer_sticky') === 'enabled') : ?>
   </div>
 <?php endif; ?>
 
+<!-- Nav Menu -->
+<div class="w-full">
+  <div class="container"><?php
+    if (has_nav_menu('footer_navigation')) :
+      wp_nav_menu([
+        'menu'            => 'nav_menu',
+        'theme_location'  => 'footer_navigation',
+        'depth'           => 1,
+        'container'       => '',
+        'container_class' => '',
+        'container_id'    => '',
+        'menu_class'      => 'footer-nav',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+      ]);
+    endif; ?>
+  </div>
+</div>
+<!-- Close Nav Menu -->
+
 <footer>
   <div class="container">
     <div class="w-full text-center"><?php
