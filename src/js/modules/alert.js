f44803c2aba28e118a5678139f13db7771a30b6d
Daniel Hewes <daniel@creativedotdesign.com>
2021-09-27 19:17:27 -0700

    feat: Remove js-cookie from Alert.js.

--- src/js/modules/alert.js
+++ src/js/modules/alert.js
@@ -1,7 +1,14 @@
-// Import Cookies
-import Cookies from 'js-cookie';
-
 const Alert = () => {
+  const getCookie = cookieName => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${cookieName}=`);
+    if (parts.length === 2)
+      return parts
+        .pop()
+        .split(';')
+        .shift();
+  };
+
   const alert = document.getElementById('tofino-notification');
 
   if (alert) {
@@ -9,17 +16,21 @@ const Alert = () => {
 
     closeIcon.addEventListener('click', e => {
       if (tofinoJS.cookieExpires) {
-        Cookies.set('tofino-notification-closed', 'yes', {
-          expires: parseInt(tofinoJS.cookieExpires, 10),
-        });
+        let date = new Date();
+
+        date.setTime(date.getTime() + parseInt(tofinoJS.cookieExpires, 10) * 24 * 60 * 60 * 1000);
+
+        const expires = 'expires=' + date.toUTCString();
+
+        document.cookie = 'tofino-notification-closed=yes;' + expires + '; path=/';
       } else {
-        Cookies.set('tofino-notification-closed', 'yes');
+        document.cookie = 'tofino-notification-closed=yes;max=age=0; path=/';
       }
 
       alert.remove();
     });
 
-    if (tofinoJS.notificationJS === 'true' && !Cookies.get('tofino-notification-closed')) {
+    if (tofinoJS.notificationJS === 'true' && !getCookie('tofino-notification-closed')) {
       // Show the notfication using JS based on the cookie (fixes html caching issue)
       alert.style.display = 'block';
     }
