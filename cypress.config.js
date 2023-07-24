aa40bade59eb720d8b57c7242823407b82c81e3a
Daniel Hewes <daniel@creativedotdesign.com>
2023-07-24 15:58:08 -0700

    test: Cypress e2e Update.

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    specPattern: 'cypress/e2e/**/*{spec,cy}.{js,ts}',
+  },
+  component: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'src/**/*{spec,cy}.{js,ts}',
+  },
+});
