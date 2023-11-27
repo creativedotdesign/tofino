3ddcc239f4b05be5bd543030968b61cd56f29ff7
Malcolm-Remple <malcolm@creativedotdesign.com>
2023-11-27 12:10:18 -0500

    feat: Sticky Footer, 404 centered

--- 404.php
+++ 404.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
-<?php _e('Sorry, but the page you were trying to view does not exist.', 'tofino'); ?>
+<main class="container flex flex-col items-center justify-center">
+  <div class="w-full text-center">
+    <span class="text-7xl">
+      404
+    </span>
+
+    <p>
+      <?php _e('Sorry, but the page you were trying to view does not exist.', 'tofino'); ?>
+    </p>
+
+    <a href="<?php echo home_url(); ?>">
+      Back to home
+    </a>
+  </div>
+</main>
 
 <?php get_footer(); ?>
