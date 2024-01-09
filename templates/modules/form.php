ed709623473851c0aef785ab1a89ca89a4cf8d72
Daniel Hewes <daniel@creativedotdesign.com>
2024-01-08 18:19:54 -0800

    feat: Update checking of Form Builder plugin to use class_exists.

--- templates/modules/form.php
+++ templates/modules/form.php
@@ -1,7 +1,7 @@
 <?php
 $form_id = get_sub_field('form_select');
 
-if (!is_plugin_active('tofino-form-builder/index.php')) {
+if (!class_exists('Tofino_Form_Builder')) {
   echo 'Please install and activate the Tofino Form Builder Plugin';
 } else {
   \Tofino\Helpers\hm_get_template_part('../../plugins/tofino-form-builder/template', ['form_id' => $form_id]);
