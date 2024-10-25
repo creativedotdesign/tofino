92b7785169933697b70a499c30bcb3be1ea15779
Daniel Hewes <daniel@creativedotdesign.com>
2024-10-25 12:03:57 -0700

    refactor: Update config files.

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+export default [
+  {
+    ignores: ['dist/**'], // Example: ignore dist folder
+  },
+  {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:vue/vue3-recommended',
+      'plugin:prettier/recommended',
+      'plugin:cypress/recommended',
+    ],
+    globals: {
+      browser: 'readonly',
+      tofinoJS: 'readonly',
+    },
+    plugins: ['@typescript-eslint'],
+    languageOptions: {
+      ecmaVersion: '2021',
+      parser: '@typescript-eslint/parser',
+      sourceType: 'module',
+    },
+    env: {
+      'vue/setup-compiler-macros': true,
+      node: true,
+      es2021: true,
+      es6: true,
+    },
+  },
+];
