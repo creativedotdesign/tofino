fd96235508c4103faa032472527e06f171be814a
Daniel Hewes <daniel@creativedotdesign.com>
2024-10-29 12:00:25 -0700

    refactor: Remove quiz specific ACF and Modules.

--- templates/modules/quiz-app.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!is_plugin_active('dci-quiz-app/index.php')) {
-  echo 'Please install and activate the DCI Quiz Plugin.';
-} else {
-  \Tofino\Helpers\hm_get_template_part('../../plugins/dci-quiz-app/template');
-}
