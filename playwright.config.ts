bf4cd9ca10c0421e3667616296688483a8cf5254
Malcolm-Remple <malcolm@creativedotdesign.com>
2025-01-15 15:00:28 -0500

    test: html validation test WIP

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /**
  * Read environment variables from file.
@@ -26,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.VITE_ASSET_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
---
+++ tests/html-validate.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import { HtmlValidate } from 'html-validate';
+
+test.only('Validate HTML', async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  // Get the HTML content
+  const html = await page.content();
+
+  // Create an html-validate instance
+  const htmlvalidate = new HtmlValidate();
+
+  // Validate the HTML
+  const report = await htmlvalidate.validateString(html);
+
+  console.log('report:', report.results);
+
+  if (report.results.length > 0) {
+    for (const item of report.results) {
+      console.log(item.messages);
+    }
+  }
+
+  // json file attachment
+  if (!report.valid) {
+    await test.info().attach('html-validate-report.json', {
+      contentType: 'application/json',
+      body: Buffer.from(JSON.stringify(report.results))
+    });
+  }
+
+  expect(report.valid, 'HTML did not pass validation').toBeTruthy();
+});
